57d1ac82c90d990153b7884dfa088d9ec19fc1d6
patrickmontalto <pmontalto@gmail.com>
2016-11-02 19:10:35 -0400

    pending photo on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	validates :caption, :user_id, :photo, presence: true
   def tags
     caption.scan(/\B#\w+/).map { |t| t[1..-1].downcase }
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,12 +5,17 @@ describe Post do
   subject { post} 
 
   it { should respond_to(:caption) }
-  it { should respond_to(:user) }
+  it { should respond_to(:user_id) }
   it { should respond_to(:tags) }
+  it { should respond_to(:photo) }
+
+  it { should validate_presence_of :caption }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :photo }
 
   describe "#tags" do
     it "returns an array of hashtags" do
-      expect post.tags.to eql %w(grateful awesome rails)
+      expect(post.tags).to eql %w(grateful awesome rails)
     end
   end
 
