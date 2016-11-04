cc79a0d45e121dbcea744189122807be24a13a2c
patrickmontalto <pmontalto@gmail.com>
2016-11-03 21:04:14 -0400

    fix photo upload directory

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,6 +3,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
      "#{secure_token(10)}.#{file.extension}" if original_filename.present?
   end
 
+  # Override the dir where the uploaded photos will be stored.
+  def store_dir
+  	"uploads/#{Rails.env}/photos"
+  end
+
   protected
   def secure_token(length=16)
     var = :"@#{mounted_as}_secure_token"
