7d17e7764c76bc300ffba15478a6b7fe369caef0
patrickmontalto <pmontalto@gmail.com>
2017-01-02 17:45:29 -0500

    config cap for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 
-set :application, 'facesnapsapi'
+set :application, 'FaceSnapsAPI'
 set :repo_url, 'git@github.com:patrickmontalto/FaceSnapsAPI.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/facesnapsapi'
+set :deploy_to, '/home/deploy/FaceSnapsAPI'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
