97aa80e5a06883efd5d44969cb3db9be1a635269
patrickmontalto <pmontalto@gmail.com>
2017-01-03 00:48:49 -0500

    temporarily remove unique index on venue_id (psql error in capistrano)

--- db/migrate/20161123190559_add_venue_id_to_locations.rb
+++ db/migrate/20161123190559_add_venue_id_to_locations.rb
@@ -2,5 +2,5 @@ class AddVenueIdToLocations < ActiveRecord::Migration
   def change
     add_column :locations, :venue_id, :string
   end
-  add_index :locations, :venue_id, unique: true
+  #add_index :locations, :venue_id, unique: true
 end
