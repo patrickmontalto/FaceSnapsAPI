610949c77e4be205bf9d1f88181c3f26e91f4c0d
patrickmontalto <pmontalto@gmail.com>
2017-01-10 13:45:02 -0500

    fix checking username or email availability

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :auth_token, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false }
   validates :full_name, presence: true
 
   has_many :posts, dependent: :destroy
