16b0b223cd07103acb1247b6486dfe043196a5a4
patrickmontalto <pmontalto@gmail.com>
2017-01-19 16:57:35 -0500

    remove base64 from serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :caption, :photo_path, :base64_image, :tags, 
+  attributes :id, :user_id, :caption, :photo_path, :tags
   has_one :user 
   has_many :comments
 end
