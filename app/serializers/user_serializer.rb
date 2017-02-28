167798338ecae515ddc2f93e6450efe81e5cb447
patrickmontalto <pmontalto@gmail.com>
2017-02-28 12:52:40 -0500

    add relationship to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :full_name, :email, :created_at, :updated_at, :auth_token, 
-  :photo_path, :following, :posts_count, :followers_count, :following_count, :private
+  :photo_path, :following, :posts_count, :followers_count, :following_count, :private, :relationship
 
   def following
     if scope.current_user.nil?
@@ -8,4 +8,14 @@ class UserSerializer < ActiveModel::Serializer
     end
     scope.current_user.following?(object)
   end
+
+  def relationship
+    if scope.current_user.nil?
+      return false
+    end
+    incoming_status = StatusReporter.outgoing(scope.current_user, object)
+    outgoing_status = StatusReporter.incoming(scope.current_user, object)
+    {incoming_status: incoming_status, outgoing_status: outgoing_status}
+  end
+
 end
